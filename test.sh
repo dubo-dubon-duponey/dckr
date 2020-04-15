e3f3c72d0b28c58719580e57b9f9ddd29bacec52
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-04-15 11:31:09 -0700

    Move to pinned versions / more versions
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- test.sh
+++ test.sh
@@ -1,55 +1,40 @@
 #!/usr/bin/env bash
+set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
-echo "Linting"
+# Linting
+. tooling/helpers.sh
+lint::shell dckr ./*.sh ./*/*.sh
+lint::dockerfile ./*Dockerfile*
+logger::info "Linting successful\n"
 
-if ! shellcheck -a -x "./test.sh"; then
-  echo "Failed linting test.sh"
-  exit 1
-fi
-
-if ! shellcheck -a -x "./dckr"; then
-  echo "Failed linting dckr"
-  exit 1
-fi
-
-if ! hadolint dckr.Dockerfile; then
-  echo "Failed linting dckr.Dockerfile"
-  exit 1
-fi
-
-if ! hadolint dckr.Dockerfile.example; then
-  echo "Failed linting dckr.Dockerfile.example"
-  exit 1
-fi
-
-echo "Testing default target"
+logger::info "Testing default target"
 
 export CACHE_DIR=cache
 
 shouldBe="Welcome to Alpine Linux 3.10"
 is="$(./dckr cat /etc/issue 2>/dev/null | grep Alpine | tr -s '\r' '\n')"
 if [ "$is" != "$shouldBe" ]; then
-  echo "Should have been: $shouldBe"
-  echo "Was: $is"
+  logger::error "Should have been: $shouldBe"
+  logger::error "Was: $is"
   exit 1
 fi
 
-echo "Testing custom target"
+logger::info "Testing custom target"
 
 shouldBe="Ubuntu 18.04.2 LTS \n \l"
 is="$(TARGET=ubuntu-lts-current ./dckr cat /etc/issue 2>/dev/null | grep Ubuntu | tr -s '\r' '\n')"
 if [ "$is" != "$shouldBe" ]; then
-  echo "Should have been: $shouldBe"
-  echo "Was: $is"
+  logger::error "Should have been: $shouldBe"
+  logger::error "Was: $is"
   exit 1
 fi
 
-echo "Testing custom dockerfile with debian target"
+logger::info "Testing custom dockerfile with debian target"
 
 shouldBe="version: 0.5.0"
 is="$(DOCKERFILE=./dckr.Dockerfile.example TARGET=debian-current ./dckr shellcheck --version  2>/dev/null | grep version: | tr -s '\r' '\n')"
 if [ "$is" != "$shouldBe" ]; then
-  echo "Should have been: $shouldBe"
-  echo "Was: $is"
+  logger::error "Should have been: $shouldBe"
+  logger::error "Was: $is"
   exit 1
 fi
